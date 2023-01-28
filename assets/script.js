18bb63d610805004f4544ac4e56b7341ff2af768
Clarissa Cole <clarissa_2006@hotmail.co.uk>
2023-01-28 20:47:50 +0000

    created questions for the user and if statements

--- assets/script.js
+++ assets/script.js
@@ -89,17 +89,25 @@ var upperCasedCharacters = [
 ];
 
 // Function to prompt user for password options
-function getPasswordOptions() {
-console.log("Press the button to create a new password");
+
+function getPasswordOptions (){
+  var getPasswordlowercase = confirm("Do you want a lowercase character?");
+  var getPassworduppercase = confirm("Do you want a Uppercase character?");
+  var getPasswordnumeric = confirm("Do you want a numeric character?");
+  var getPasswordspecial = confirm("Do you want a special character character?");
+  var getPasswordOptions = 0;
+  if(getPasswordlowercase) getPasswordOptions++;
+  if(getPassworduppercase) getPasswordOptions++;
+  if(getPasswordnumeric) getPasswordOptions++;
+  if(getPasswordspecial) getPasswordOptions++;
 }
-var password = prompt("We will generate a password with special character, press to continue")
 
 // Function for getting a random element from an array
 function getRandom(arr) 
-for (var i = 0; i < arr.length; i++) {
+// for (var i = 0; i < arr.length; i++) {
 
 
-}
+// }
 
 
 
@@ -107,9 +115,13 @@ for (var i = 0; i < arr.length; i++) {
 // }
 
 // Function to generate password with user input
-function generatePassword() {
+function generatePassword (
 
-}
+
+for (var j = 0; i < getPasswordOptions; j++) 
+var password = generatePassword(getPasswordlowercase,getPassworduppercase,getPasswordnumeric,getPasswordspecial)
+var passwordchoice = generatePassword[Math.floor(Math.random() * getPasswordOptions.length)];
+)
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -124,3 +136,22 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+//  var uppercasedCharacters=0
+//  var specialCharacters=0
+//  var numericCharacters=0
+//  var lowerCasedCharacters=0
+
+
+// function getPasswordOptions (){
+//   lowerCasedCharacters = confirm("Do you want a lowercase character?");
+//   upperCasedCharacters = confirm("Do you want a Uppercase character?");
+//    numericCharacters = confirm("Do you want a numeric character?");
+//    specialCharacters = confirm("Do you want a special character character?");
+//   // var getPasswordOptions = getPasswordlowercase + getPasswordnumeric + getPasswordnumeric + getPasswordspecial
+//   }
+//   var getPasswordOptions = 0;
+//   if(getPasswordlowercase) getPasswordOptions++;
+//   if(getPassworduppercase) getPasswordOptions++;
+//   if(getPasswordnumeric) getPasswordOptions++;
+//   if(getPasswordspecial) getPasswordOptions++;
